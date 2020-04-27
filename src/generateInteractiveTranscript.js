72dd0b6e542d800b313af3d02910513d890be455
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-04-27 19:44:11 -0400

    added slate-transcript-editor and tweaked the UI

---
+++ src/generateInteractiveTranscript.js
@@ -0,0 +1,2 @@
+const generateInteractiveTranscript = require('./generate-html-to-check-alignement/index.js');
+module.exports.generateInteractiveTranscript = generateInteractiveTranscript;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 const addTimecodesToQuotes = require('./add-timecodes-to-quotes/index.js');
-const generateInteractiveTranscript = require('./generate-html-to-check-alignement/index.js');
-
 module.exports = addTimecodesToQuotes;
 module.exports.addTimecodesToQuotes = addTimecodesToQuotes;
-module.exports.generateInteractiveTranscript = generateInteractiveTranscript;
